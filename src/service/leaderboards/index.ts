d599fc6d4678babf2c3bf764f9c05b33abba493f
ZnovandaP <znovandaputra@gmail.com>
2024-02-21 17:30:35 +0700

    feat: add new function as a service for touch to api leaderboards forum api

---
+++ src/service/leaderboards/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { baseUrl } from '../common/fetch-with-auth';
+
+const getLeaderboards = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/leaderboard`);
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
+
+export default getLeaderboards;
