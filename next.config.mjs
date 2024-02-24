1c00c11637b6e6e79a638e53ca602e450db29dae
ZnovandaP <znovandaputra@gmail.com>
2024-02-24 16:49:39 +0700

    feat(next config): add strictmode, images config and many things

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "", 
+        pathname: "/**"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
