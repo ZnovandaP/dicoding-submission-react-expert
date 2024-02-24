939209d0dbd25dc59284f8fb86db7800316a0576
ZnovandaP <znovandaputra@gmail.com>
2024-02-24 16:55:08 +0700

    refactor(NextThemeProvider): change name component provider

--- src/components/NextThemeProvider/index.tsx
+++ src/components/NextThemeProvider/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+export default function NextThemesProvider({ children, ...props }: ThemeProviderProps) {
+  return <ThemeProvider {...props}>{children}</ThemeProvider>;
 }
