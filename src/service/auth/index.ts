a5dea9a8aeba2dcafcf158f3d003a1b42efcfc3f
ZnovandaP <znovandaputra@gmail.com>
2024-02-25 18:01:34 +0700

    refactor(service auth): remove check input before auth, because have been handle by zod

--- src/service/auth/index.ts
+++ src/service/auth/index.ts
@@ -12,13 +12,6 @@ export type RegisterParams = LoginParams & {
 
 export const login = async ({ email, password }: LoginParams) => {
   try {
-    if (!email || !password) {
-      throw new Error('Email dan password sangat dibutuhkan');
-    }
-
-    if (password.length < 6) {
-      throw new Error('Minimal 6 karakter password');
-    }
     const body = { email, password };
     const { data } = await axios.post(`${baseUrl}/login`, body);
     return data;
@@ -29,16 +22,14 @@ export const login = async ({ email, password }: LoginParams) => {
 
 export const register = async ({ email, password, name }: RegisterParams) => {
   try {
-    if (!email || !password || !name) {
-      throw new Error('Email dan password sangat dibutuhkan');
+    const body = { email, password, name };
+    const response = await axios.post(`${baseUrl}/register`, body);
+
+    if (response.status === 400) {
+      throw new Error(response.data.message);
     }
 
-    if (password.length < 6) {
-      throw new Error('Minimal 6 karakter password');
-    }
-    const body = { email, password, name };
-    const { data } = await axios.post(`${baseUrl}/register`, body);
-    return data;
+    return response.data;
   } catch (error: any) {
     throw new Error(error.message);
   }
