d6de610145bacd12ac64a8a173f4a77d033911ef
ZnovandaP <znovandaputra@gmail.com>
2024-02-25 18:35:03 +0700

    feat(RegisterViews): create component RegisterViews for mounting on page.tsx aka register page

---
+++ src/views/AuthViews/RegisterViews/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import * as React from 'react';
+import AuthWrapper from '../ComponentSupport/AuthWrapper';
+import AuthNote from '../ComponentSupport/AuthNote';
+import FormRegister from '../ComponentSupport/FormRegister';
+import AuthTitle from '../ComponentSupport/AuthTitle';
+
+export default function RegisterViews() {
+  return (
+    <AuthWrapper position="right">
+      <AuthTitle title="Register" />
+      <FormRegister />
+      <AuthNote
+        message="Sudah punya akun? silahkan"
+        messageLink="login disini"
+        to="/login"
+      />
+    </AuthWrapper>
+  );
+}
