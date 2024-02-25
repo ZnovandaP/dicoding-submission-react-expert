fbfccd9a6c42c38d26de8c77165e76ae16464287
ZnovandaP <znovandaputra@gmail.com>
2024-02-25 18:14:21 +0700

    feat(Input Comp): create component Input for auth page

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import * as React from 'react';
+import { ErrorMessage } from '@hookform/error-message';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label: string
+  id?: string
+  error: object
+  type: React.InputHTMLAttributes<HTMLInputElement>['type']
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({
+  label, id = '', error, type, ...props
+}, ref) => (
+  <>
+    {' '}
+    <label htmlFor={id} className="flex flex-col gap-2">
+      {' '}
+      {label}
+      <input
+        {...props}
+        ref={ref}
+        type={type}
+        name={id}
+        id={id}
+        required
+        className="rounded-md bg-transparent p-3 ring-1 ring-primary/60 focus:outline-none focus:ring-2 focus:ring-primary"
+      />
+    </label>
+
+    <ErrorMessage
+      name={id}
+      errors={error}
+      render={({ message }) => <p className="text-red-600 dark:text-red-500">{message}</p>}
+    />
+  </>
+));
+
+export default Input;
