093b6e32a7ace7cb6aa839fd9c93ebb68446f243
ZnovandaP <znovandaputra@gmail.com>
2024-02-26 12:27:41 +0700

    feat (add validation button submit auth): add condition/valdation button is disable  when user input not match in validation

--- src/views/AuthViews/ComponentSupport/ButtonAuthSubmit.tsx
+++ src/views/AuthViews/ComponentSupport/ButtonAuthSubmit.tsx
@@ -4,26 +4,33 @@ import * as React from 'react';
 import Button from '@/components/Button';
 import { ImSpinner9 } from 'react-icons/im';
 import { useAppSelector } from '@/libs/redux/store';
+import { FieldErrors } from 'react-hook-form';
 
 type ButtonAuthSubmitProps = {
   isSubmitting: boolean
   label: string
+  errors: FieldErrors
 };
 
-export default function ButtonAuthSubmit({ isSubmitting, label }: ButtonAuthSubmitProps) {
-  const { status, message } = useAppSelector((state) => state.register);
+export default function ButtonAuthSubmit({ isSubmitting, label, errors }: ButtonAuthSubmitProps) {
+  const register = useAppSelector((state) => state.register);
+  const login = useAppSelector((state) => state.login);
+
+  const checkinInputValidationIsError = Object.keys(errors).length > 0;
+  const isLoading = register.status === 'loading' || login.status === 'loading';
+  const isDisable = isSubmitting || isLoading || checkinInputValidationIsError;
 
   return (
     <Button
       type="submit"
       variant="primary"
       className="center gap-3 mt-4 disabled:cursor-not-allowed disabled:opacity-50"
-      disabled={isSubmitting || status === 'loading'}
+      disabled={isDisable}
     >
-      {status === 'loading' && (
+      {isLoading && (
       <ImSpinner9 className="text-xl text-neutral-50 animate-spin" />
       )}
-      {status === 'loading' ? message : label}
+      {isLoading ? 'Loading...' : label}
     </Button>
   );
 }
