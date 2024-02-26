54699a4b9a93832e6c19e8c57d1dddbd3cc70cea
ZnovandaP <znovandaputra@gmail.com>
2024-02-26 12:46:29 +0700

    feat(trigger loadingBar): implement loadingBar could be trigger when fetchin data  on registerSlice

--- src/libs/redux/slices/auth/register.ts
+++ src/libs/redux/slices/auth/register.ts
@@ -1,18 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { register, RegisterParams } from '@/service/auth';
 import { toast } from 'react-toastify';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
 
 type InitialState = {
   message: string | null
   status: 'idle' | 'loading' | 'error' | 'success';
 };
 
-export const asyncRegister = createAsyncThunk('auth/register', async (body: RegisterParams) => {
+export const asyncRegister = createAsyncThunk('auth/register', async (body: RegisterParams, { dispatch }) => {
   try {
+    dispatch(showLoading());
     const data = await register(body);
     return data;
   } catch (error: any) {
     throw new Error(error.message);
+  } finally {
+    dispatch(hideLoading());
   }
 });
 
