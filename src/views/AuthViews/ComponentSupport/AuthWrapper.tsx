6e7b447ff6926bd40e3e4eb35ed93a3fe5f44ed0
ZnovandaP <znovandaputra@gmail.com>
2024-02-26 12:25:40 +0700

    feat (mobile view auth wrapper): change width auth wrapper to be 100% on mobile view

--- src/views/AuthViews/ComponentSupport/AuthWrapper.tsx
+++ src/views/AuthViews/ComponentSupport/AuthWrapper.tsx
@@ -10,7 +10,7 @@ export default function AuthWrapper({ children, position = 'left' }: AuthWrapper
   return (
     <div
       className={cn(
-        'absolute left-1/2 top-1/2 flex w-[85%] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4 rounded-md bg-neutral-200 p-8 dark:bg-neutral-800 md:top-0 md:h-full md:w-2/3 md:-translate-x-0 md:-translate-y-0 md:justify-center lg:w-1/2 transition-all duration-300 animate-fade',
+        'absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4 rounded-md bg-neutral-200 p-8 dark:bg-neutral-800 sm:w-[85%] md:top-0 md:h-full md:w-2/3 md:-translate-x-0 md:-translate-y-0 md:justify-center lg:w-1/2 transition-all duration-300 animate-fade',
 
         position === 'left' && 'md:left-0 md:animate-half-slide-left',
 
