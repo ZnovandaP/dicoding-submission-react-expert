7fdfcb9a94cf78acbc38ff3dce129a420bc02af1
ZnovandaP <znovandaputra@gmail.com>
2024-02-26 13:00:51 +0700

    feat(object data metadata): create constant object data metadata

---
+++ src/constant/metadata.ts
@@ -0,0 +1,17 @@
+const METADATA = {
+  creator: 'Zidane Novanda Putra',
+  description: 'Sharing Sepuh merupakan platform diskusi untuk berbagai hal topik yang sedang happening banget.',
+  keyword: 'Sharing Sepuh, diskusi, forum diskusi, sharingsepuh',
+  authors: {
+    name: 'Zidane Novanda Putra',
+    url: process.env.DOMAIN,
+  },
+  openGraph: {
+    url: process.env.DOMAIN,
+    siteName: 'Sharing Sepuh',
+    locale: 'id-ID',
+  },
+  exTitle: '| Sharing Sepuh',
+};
+
+export default METADATA;
