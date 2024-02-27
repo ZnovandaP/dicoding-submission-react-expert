383caa0742c42061a5178b7f870561857852aab5
ZnovandaP <znovandaputra@gmail.com>
2024-02-27 14:15:29 +0700

    feat(add condition rendering) conditional rendering base on user is Auth on NavMenu and NavMenuDrawer

--- src/components/Navbar/NavMenu/NavMenu.tsx
+++ src/components/Navbar/NavMenu/NavMenu.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import ButtonNavAuth from '@/components/Button/ButtonNavAuth';
 import ButtonNavMenu from '@/components/Button/ButtonNavMenu';
 import NavLink from '@/components/Navbar/NavLink';
 import dataNavbar from '@/constant/data-nav';
+import { useSession } from 'next-auth/react';
 import * as React from 'react';
 
 type NavMenuProps = {
@@ -9,14 +13,35 @@ type NavMenuProps = {
 };
 
 export default function NavMenu({ open, setOpen }: NavMenuProps) {
+  const { status } = useSession();
+
+  const isNotAuth = status === 'unauthenticated';
   return (
     <>
       <ul className="hidden md:flex items-center justify-evenly gap-6">
-        {dataNavbar.map((data) => (
-          <li key={data.name}>
-            <NavLink to={data.to}>{data.name}</NavLink>
-          </li>
-        ))}
+        {dataNavbar.map((data) => {
+          if (data.name === 'Login') {
+            return isNotAuth && (
+            <li key={data.name}>
+              <ButtonNavAuth to={data.to}>{data.name}</ButtonNavAuth>
+            </li>
+            );
+          }
+
+          if (data.name === 'Register') {
+            return isNotAuth && (
+            <li key={data.name}>
+              <NavLink to={data.to}>{data.name}</NavLink>
+            </li>
+            );
+          }
+
+          return (
+            <li key={data.name}>
+              <NavLink to={data.to}>{data.name}</NavLink>
+            </li>
+          );
+        })}
         <li>
           <ButtonNavMenu open={open} setOpen={setOpen} />
         </li>
--- src/components/Navbar/NavMenu/NavMenuDrawer.tsx
+++ src/components/Navbar/NavMenu/NavMenuDrawer.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import * as React from 'react';
 import DrawerBase from '@/components/Drawer';
 import NavLink from '@/components/Navbar/NavLink';
 import dataNavbar from '@/constant/data-nav';
+import ButtonNavAuth from '@/components/Button/ButtonNavAuth';
+import { useSession } from 'next-auth/react';
 import ProfileNavMenu from './ProfileNavMenu';
 import ThemesMenu from './ThemesMenu';
 
@@ -10,20 +14,42 @@ type NavMenuProps = {
 };
 
 export default function NavMenuDrawer({ open }: NavMenuProps) {
+  const { status } = useSession();
+
+  const isNotAuth = status === 'unauthenticated';
+
   return (
     <DrawerBase
       open={open}
-      className="absolute right-8 top-24 min-w-56 max-w-72 p-6 ring-1 ring-primary bg-neutral-100 dark:bg-neutral-900 max-h-[580px] overflow-auto"
+      className="absolute right-8 top-24 w-72 p-6 ring-1 ring-primary bg-neutral-100 dark:bg-neutral-900 max-h-[580px] overflow-auto"
     >
       <ul className="flex w-full flex-col gap-4">
         <li>
           <ProfileNavMenu />
         </li>
-        {dataNavbar.map((data) => (
-          <li key={data.name} className="md:hidden">
-            <NavLink to={data.to}>{data.name}</NavLink>
-          </li>
-        ))}
+        {dataNavbar.map((data) => {
+          if (data.name === 'Login') {
+            return isNotAuth && (
+            <li key={data.name} className="md:hidden">
+              <ButtonNavAuth to={data.to}>{data.name}</ButtonNavAuth>
+            </li>
+            );
+          }
+
+          if (data.name === 'Register') {
+            return isNotAuth && (
+            <li key={data.name} className="md:hidden">
+              <NavLink to={data.to}>{data.name}</NavLink>
+            </li>
+            );
+          }
+
+          return (
+            <li key={data.name} className="md:hidden">
+              <NavLink to={data.to}>{data.name}</NavLink>
+            </li>
+          );
+        })}
         <li>
           <ThemesMenu />
         </li>
