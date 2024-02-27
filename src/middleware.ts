428c19721dcf21b167e70bc8ea817b6870a5af5f
ZnovandaP <znovandaputra@gmail.com>
2024-02-27 15:03:49 +0700

    feat(add middleware.ts): middleware for protect route whether user is auth?

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+import { NextResponse, NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const arrayOfPathname = pathname.split('/');
+  const currentPathname = `/${arrayOfPathname[1]}`;
+
+  // * protection routes user not auth
+  const tokenAuth = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const routeProtectAuth = ['/login', '/register', '/auth'];
+
+  if (tokenAuth) {
+    if (routeProtectAuth.includes(currentPathname)) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
