1acaeca62a3a0bfbcff54fa665cb300246b920e4
ZnovandaP <znovandaputra@gmail.com>
2024-02-29 11:05:35 +0700

    feat(slice get threads and delete vote slices): assemble logic vote thread to getThreadsSlice and deleted voteSlices

--- src/libs/redux/slices/threads/get-threads.ts
+++ src/libs/redux/slices/threads/get-threads.ts
@@ -1,21 +1,141 @@
+import { toast } from 'react-toastify';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import { getAllUsers } from '@/service/users';
 import { getAllThreads } from '@/service/threads';
-import type { Threads } from '@/types/response/threads';
+import { downVoteThread, neutralVoteThread, upVoteThread } from '@/service/votes/thread';
+import { Users } from '@/types/response/users';
+import { Vote } from '@/types/response/vote';
+import type { ThreadWithAuthor, Threads } from '@/types/response/threads';
 
 type InitialState = {
-  data: Threads | null
+  data: ThreadWithAuthor[] | null
   message: string | null
   status: 'idle' | 'loading' | 'error' | 'success';
 };
 
-export const asyncGetThreads = createAsyncThunk('thread/getThreads', async () => {
+export const asyncGetThreadsWithAuthor = createAsyncThunk('thread/getThreadsWithAuthor', async () => {
   try {
-    const { data } = await getAllThreads();
-    return data;
+    const { data: dataThreads } = await getAllThreads();
+    const { data: dataUsers } = await getAllUsers();
+
+    const threads = dataThreads.threads as Threads;
+    const users = dataUsers.users as Users;
+
+    return threads.map((thread) => {
+      const author = users.find((user) => thread.ownerId === user.id);
+
+      if (typeof author === 'undefined') return thread;
+
+      return {
+        ...thread,
+        author: {
+          name: author.name,
+          email: author.email,
+          avatar: author.avatar,
+        },
+      };
+    }) as ThreadWithAuthor[];
   } catch (error: any) {
     throw new Error(error.message);
   }
 });
+
+export const asyncUpVoteThread = createAsyncThunk(
+  'thread/upVote',
+  async (threadId: string, { dispatch, getState }) => {
+    try {
+      dispatch(showLoading());
+      await upVoteThread(threadId);
+
+      const { threads } = getState() as { threads: { data: ThreadWithAuthor[] } };
+      const { profile } = getState() as { profile: { data: Vote } };
+
+      const profileId = profile.data?.id;
+
+      return threads.data?.map((thread) => {
+        if (thread.id === threadId) {
+          return {
+            ...thread,
+            upVotesBy: thread.upVotesBy.includes(profileId)
+              ? thread.upVotesBy
+              : [...thread.upVotesBy, profileId],
+            downVotesBy: thread.downVotesBy.includes(profileId)
+              ? thread.downVotesBy.filter((voteId) => voteId !== profileId)
+              : thread.downVotesBy,
+          };
+        }
+        return thread;
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    } finally {
+      dispatch(hideLoading());
+    }
+  },
+);
+
+export const asyncDownVoteThread = createAsyncThunk(
+  'thread/downVote',
+  async (threadId: string, { dispatch, getState }) => {
+    try {
+      dispatch(showLoading());
+      await downVoteThread(threadId);
+
+      const { threads } = getState() as { threads: { data: ThreadWithAuthor[] } };
+      const { profile } = getState() as { profile: { data: Vote } };
+
+      const profileId = profile.data?.id;
+
+      return threads.data?.map((thread) => {
+        if (thread.id === threadId) {
+          return {
+            ...thread,
+            upVotesBy: thread.upVotesBy.includes(profileId)
+              ? thread.upVotesBy.filter((voteId) => voteId !== profileId)
+              : thread.upVotesBy,
+            downVotesBy: thread.downVotesBy.includes(profileId)
+              ? thread.downVotesBy
+              : [...thread.downVotesBy, profileId],
+          };
+        }
+        return thread;
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    } finally {
+      dispatch(hideLoading());
+    }
+  },
+);
+
+export const asyncNeutralVoteThread = createAsyncThunk(
+  'thread/NeutralVote',
+  async (threadId: string, { dispatch, getState }) => {
+    try {
+      dispatch(showLoading());
+      const { threads } = getState() as any;
+      const { data }: { data: { vote: Vote } } = await neutralVoteThread(threadId);
+
+      const threadWithAuthor = threads.data as ThreadWithAuthor[];
+
+      return threadWithAuthor.map((thread) => {
+        if (thread.id === threadId) {
+          return {
+            ...thread,
+            upVotesBy: thread.upVotesBy.filter((voteId) => voteId !== data.vote.userId),
+            downVotesBy: thread.downVotesBy.filter((voteId) => voteId !== data.vote.userId),
+          };
+        }
+        return thread;
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    } finally {
+      dispatch(hideLoading());
+    }
+  },
+);
 
 const initialState: InitialState = {
   data: null,
@@ -24,25 +144,50 @@ const initialState: InitialState = {
 };
 
 const getThreadsSlice = createSlice({
-  name: 'asyncGetThreads',
+  name: 'asyncGetThreadsWithAuthor',
   initialState,
   reducers: {},
   extraReducers(builder) {
     builder
-      .addCase(asyncGetThreads.pending, (state) => {
+      .addCase(asyncGetThreadsWithAuthor.pending, (state) => {
         state.status = 'loading';
         state.message = 'Mohon tunggu...';
       })
 
-      .addCase(asyncGetThreads.fulfilled, (state, action) => {
-        state.data = action.payload.threads as Threads;
+      .addCase(asyncGetThreadsWithAuthor.fulfilled, (state, action) => {
+        state.data = action.payload;
         state.status = 'success';
         state.message = 'Data threads berhasil didapatkan!';
       })
 
-      .addCase(asyncGetThreads.rejected, (state) => {
+      .addCase(asyncUpVoteThread.fulfilled, (state, action) => {
+        state.data = action.payload;
+      })
+
+      .addCase(asyncDownVoteThread.fulfilled, (state, action) => {
+        state.data = action.payload;
+      })
+
+      .addCase(asyncNeutralVoteThread.fulfilled, (state, action) => {
+        state.data = action.payload;
+      })
+
+      .addCase(asyncUpVoteThread.rejected, (state) => {
         state.status = 'error';
-        state.message = 'Data threads gagal didapatkan!';
+        state.message = 'Thread tidak ditemukan! Silahkan refresh';
+        toast.error(state.message);
+      })
+
+      .addCase(asyncDownVoteThread.rejected, (state) => {
+        state.status = 'error';
+        state.message = 'Thread tidak ditemukan! Silahkan refresh';
+        toast.error(state.message);
+      })
+
+      .addCase(asyncNeutralVoteThread.rejected, (state) => {
+        state.status = 'error';
+        state.message = 'Thread tidak ditemukan! Silahkan refresh';
+        toast.error(state.message);
       });
   },
 
