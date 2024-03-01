beadb036a50814b04595d663371c5cc4adf3ffd0
ZnovandaP <znovandaputra@gmail.com>
2024-03-01 13:33:56 +0700

    feat(mount component ButtonCreatePost): Mount component ButtonCreatePost on component ThreadViews.tsx

--- src/views/ThreadsViews/index.tsx
+++ src/views/ThreadsViews/index.tsx
@@ -6,13 +6,14 @@ import { useAppSelector } from '@/libs/redux/store';
 import { asyncGetProfile } from '@/libs/redux/slices/users/get-own-profile';
 import { asyncGetThreadsWithAuthor } from '@/libs/redux/slices/threads/get-threads';
 import ThreadsContent from './ComponentSupport/ThreadsContent';
+import ButtonCreatePost from './ComponentSupport/ButtonCreatePost';
 
 export default function ThreadsViews() {
   usePreload([asyncGetThreadsWithAuthor, asyncGetProfile]);
   const { data } = useAppSelector((state) => state.threads);
 
   return (
-    <section className="mt-8 pb-8">
+    <section className="mt-8 pb-8 relative min-h-[80dvh]">
       <h2 className="font-kaushan font-semibold text-3xl md:text-4xl">
         Diskusi yang tersedia
         <span className="text-primary dark:text-dark-primary ml-2">
@@ -21,6 +22,8 @@ export default function ThreadsViews() {
       </h2>
 
       <ThreadsContent />
+      <ButtonCreatePost />
     </section>
+
   );
 }
