5e218070600267ca31edca8b64529dc2a4778c92
ZnovandaP <znovandaputra@gmail.com>
2024-03-03 00:11:15 +0700

    feat(create compoenent ButtonThreadAction collection): create component ButtonThreadAction for thrads and detail thread absolutly with the logic and handling

---
+++ src/components/Button/ButtonVotesCommentThread.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { useSession } from 'next-auth/react';
+import {
+  asyncUpVoteCommentThread,
+  asyncDownVoteCommentThread,
+  asyncNeutralVoteCommentThread,
+} from '@/libs/redux/slices/detail-thread/vote-comment-thread';
+import { toast } from 'react-toastify';
+import { useAppDispatch, useAppSelector } from '@/libs/redux/store';
+import { CommentWithEmailOwner } from '@/types/response/comment';
+import { ButtonUpVote, ButtonDownVote } from './ButtonThreadAction';
+
+export default function ButtonVotesCommentThread({ comment }: { comment: CommentWithEmailOwner }) {
+  const { status } = useSession();
+  const dispatch = useAppDispatch();
+  const { data: profile } = useAppSelector((state) => state.profile);
+
+  const isUpVoted = comment.upVotesBy.includes(profile?.id as string);
+  const handleUpVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+      return;
+    }
+
+    if (isUpVoted) {
+      dispatch(asyncNeutralVoteCommentThread(comment?.id));
+    } else {
+      dispatch(asyncUpVoteCommentThread(comment.id));
+    }
+  };
+
+  const isDownVoted = comment.downVotesBy.includes(profile?.id as string);
+  const handleDownVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+    }
+
+    if (isDownVoted) {
+      dispatch(asyncNeutralVoteCommentThread(comment.id));
+    } else {
+      dispatch(asyncDownVoteCommentThread(comment.id));
+    }
+  };
+  return (
+    <>
+      <ButtonUpVote
+        onClick={handleUpVote}
+        countVote={comment.upVotesBy.length}
+        isVoted={isUpVoted}
+      />
+      <ButtonDownVote
+        onClick={handleDownVote}
+        countVote={comment.downVotesBy.length}
+        isVoted={isDownVoted}
+      />
+    </>
+  );
+}
---
+++ src/components/Button/ButtonVotesThread.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import { useSession } from 'next-auth/react';
+import { useAppDispatch, useAppSelector } from '@/libs/redux/store';
+import {
+  asyncNeutralVoteThread,
+  asyncUpVoteThread,
+  asyncDownVoteThread,
+} from '@/libs/redux/slices/threads/vote-threads-thunk';
+import {
+  asyncNeutralVoteDetailThread,
+  asyncUpVoteDetailThread,
+  asyncDownVoteDetailThread,
+} from '@/libs/redux/slices/detail-thread/vote-detail-thread-thunk';
+import { ThreadWithOwner, DetailThreadWithEmailOwner } from '@/types/response/threads';
+import { toast } from 'react-toastify';
+import { ButtonUpVote, ButtonDownVote } from './ButtonThreadAction';
+
+export function ButtonVotesThreads({ thread }:
+{ thread: ThreadWithOwner | DetailThreadWithEmailOwner }) {
+  const { status } = useSession();
+  const dispatch = useAppDispatch();
+  const { data: profile } = useAppSelector((state) => state.profile);
+
+  const isUpVoted = thread.upVotesBy.includes(profile?.id as string);
+  const handleUpVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+      return;
+    }
+
+    if (isUpVoted) {
+      dispatch(asyncNeutralVoteThread(thread?.id));
+    } else {
+      dispatch(asyncUpVoteThread(thread.id));
+    }
+  };
+
+  const isDownVoted = thread.downVotesBy.includes(profile?.id as string);
+  const handleDownVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+    }
+
+    if (isDownVoted) {
+      dispatch(asyncNeutralVoteThread(thread.id));
+    } else {
+      dispatch(asyncDownVoteThread(thread.id));
+    }
+  };
+  return (
+    <>
+      <ButtonUpVote
+        onClick={handleUpVote}
+        countVote={thread.upVotesBy.length}
+        isVoted={isUpVoted}
+      />
+      <ButtonDownVote
+        onClick={handleDownVote}
+        countVote={thread.downVotesBy.length}
+        isVoted={isDownVoted}
+      />
+    </>
+  );
+}
+export function ButtonVotesDetailThread({ thread }:
+{ thread: ThreadWithOwner | DetailThreadWithEmailOwner }) {
+  const { status } = useSession();
+  const dispatch = useAppDispatch();
+  const { data: profile } = useAppSelector((state) => state.profile);
+
+  const isUpVoted = thread.upVotesBy.includes(profile?.id as string);
+  const handleUpVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+      return;
+    }
+
+    if (isUpVoted) {
+      dispatch(asyncNeutralVoteDetailThread(thread?.id));
+    } else {
+      dispatch(asyncUpVoteDetailThread(thread.id));
+    }
+  };
+
+  const isDownVoted = thread.downVotesBy.includes(profile?.id as string);
+  const handleDownVote = () => {
+    if (status === 'unauthenticated') {
+      toast.error('Anda harus login terlebih dahulu');
+    }
+
+    if (isDownVoted) {
+      dispatch(asyncNeutralVoteDetailThread(thread.id));
+    } else {
+      dispatch(asyncDownVoteDetailThread(thread.id));
+    }
+  };
+
+  return (
+    <>
+      <ButtonUpVote
+        onClick={handleUpVote}
+        countVote={thread.upVotesBy.length}
+        isVoted={isUpVoted}
+      />
+      <ButtonDownVote
+        onClick={handleDownVote}
+        countVote={thread.downVotesBy.length}
+        isVoted={isDownVoted}
+      />
+    </>
+  );
+}
