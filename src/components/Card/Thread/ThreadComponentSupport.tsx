120e2102d00ce349dbfbf3093ee58ced0141d684
ZnovandaP <znovandaputra@gmail.com>
2024-03-03 00:13:41 +0700

    feat(adding some condition in ThreadComponentSupport): add some condition rendering so it can be use on threads and detail thread

--- src/components/Card/Thread/ThreadComponentSupport.tsx
+++ src/components/Card/Thread/ThreadComponentSupport.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import * as React from 'react';
-import { ThreadWithAuthor } from '@/types/response/threads';
+import { DetailThreadWithEmailOwner, ThreadWithOwner } from '@/types/response/threads';
 import Avatar from '@/components/Avatar';
-import { useParseTimeToNow } from '@/hooks/useFormatTime';
 import parse from 'html-react-parser';
 import Link from 'next/link';
 import Badge from '@/components/Badge';
-import { ButtonDownVote, ButtonUpVote, ButtonGoToComment } from '@/components/Button/ButtonThreadAction';
+import { useParseTimeToNow, useFormatDate } from '@/hooks/useFormatTime';
+import { ButtonGoToComment } from '@/components/Button/ButtonThreadAction';
+import {
+  ButtonVotesThreads, ButtonVotesDetailThread,
+} from '@/components/Button/ButtonVotesThread';
 
 export type ThreadComponentSupportProps = {
-  thread: ThreadWithAuthor;
+  thread: ThreadWithOwner | DetailThreadWithEmailOwner;
 };
 
 export function ThreadWrapper({ children }: { children: React.ReactNode }) {
@@ -23,48 +26,61 @@ export function ThreadWrapper({ children }: { children: React.ReactNode }) {
   );
 }
 
-export function ThreadHead({ thread }: ThreadComponentSupportProps) {
+export function ThreadHead({ thread, isDetailThread = false }
+: ThreadComponentSupportProps & { isDetailThread?: boolean }) {
   const parseTimeToNow = useParseTimeToNow();
+  const formatDate = useFormatDate();
 
   return (
     <div className="flex justify-between ">
       <div className="flex gap-4 items-center">
         <Avatar
-          src={thread.author.avatar ?? '/user.png'}
-          alt={`Avatar ${thread.author.name ?? 'user'}`}
+          src={thread.owner.avatar ?? '/user.png'}
+          alt={`Avatar ${thread.owner.name ?? 'user'}`}
         />
         <div className="flex flex-col">
           <p className="line-clamp-1">
-            {thread.author.name ?? 'Nama pengguna tidak ditemukan'}
+            {thread.owner.name ?? 'Nama pengguna tidak ditemukan'}
           </p>
           <p className="text-sm opacity-50 line-clamp-1">
-            {thread.author.email ?? 'Email pengguna tidak ditemukan'}
+            {thread.owner.email ?? 'Email pengguna tidak ditemukan'}
           </p>
         </div>
       </div>
       <p className="text-sm opacity-50 line-clamp-1 h-5">
-        {parseTimeToNow(thread.createdAt)}
+        {
+          !isDetailThread ? parseTimeToNow(thread.createdAt) : formatDate(thread.createdAt)
+        }
       </p>
     </div>
   );
 }
 
-export function ThreadBody({ thread }: ThreadComponentSupportProps) {
+export function ThreadBody({ thread, isDetailThread = false }
+: ThreadComponentSupportProps & { isDetailThread?: boolean }) {
   return (
     <div className="ml-12 flex flex-col gap-3">
+      {!isDetailThread && (
       <Link
         href={`/thread/${thread.id}`}
-        className="text-lg text-primary dark:text-dark-primary underline-offset-8 decoration-wavy decoration-1 hover:underline"
+        className="text-lg text-primary dark:text-dark-primary underline-offset-8 decoration-wavy decoration-1 first-letter:uppercase hover:underline"
       >
         {thread.title}
       </Link>
+      )}
 
-      <div className="line-clamp-5">
-        {parse(thread.body)}
-      </div>
+      {isDetailThread ? (
+        <div className="">
+          {parse(thread.body)}
+        </div>
+      ) : (
+        <div className="line-clamp-5">
+          {parse(thread.body)}
+        </div>
+      )}
 
       <div className="flex gap-2 items-center md:hidden mt-2">
-        <Badge>
+        <Badge size="md">
           {`#${thread.category}`}
         </Badge>
       </div>
@@ -73,20 +89,25 @@ export function ThreadBody({ thread }: ThreadComponentSupportProps) {
   );
 }
 
-export function ThreadFoot({ thread }: ThreadComponentSupportProps) {
+export function ThreadFoot(
+  { thread, isDetailThread = false }: ThreadComponentSupportProps & { isDetailThread?: boolean },
+) {
   return (
     <div className="flex justify-center md:justify-between items-center mt-4 md:ml-12">
       <div className="flex items-center gap-3 ">
-        <ButtonUpVote thread={thread} />
 
-        <ButtonDownVote thread={thread} />
+        {!isDetailThread && <ButtonVotesThreads thread={thread} /> }
+        {isDetailThread && <ButtonVotesDetailThread thread={thread} /> }
 
-        <ButtonGoToComment commentCount={thread.totalComments} threadId={thread.id} />
+        <ButtonGoToComment
+          commentCount={thread.totalComments}
+          threadId={thread.id}
+        />
 
       </div>
 
       <div className="hidden gap-2 items-center md:flex">
-        <Badge>
+        <Badge size="md">
           {`#${thread.category}`}
         </Badge>
       </div>
