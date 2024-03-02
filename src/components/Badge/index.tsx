1fa8404b377ddbd05bc957e440498c371917fd21
ZnovandaP <znovandaputra@gmail.com>
2024-03-03 00:06:47 +0700

    feat(add props size): implement option size through props its gonna render depends props size

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,12 +1,22 @@
+import cn from '@/utils/cn';
 import * as React from 'react';
 
 type BadgeProps = {
   children: React.ReactNode
+  size?: 'sm' | 'md' | 'lg' | 'xl',
+  className?: string
 };
 
-export default function Badge({ children }: BadgeProps) {
+export default function Badge({ children, size = 'sm', className }: BadgeProps) {
   return (
-    <div className="center text-xs px-4 py-1 ring-1 ring-primary rounded-full">
+    <div className={cn(
+      size === 'sm' && 'center text-xs px-4 py-1 ring-1 ring-primary rounded-full',
+
+      size === 'md' && 'center text-base px-4 py-2 ring-1 ring-primary rounded-full',
+
+      className ?? '',
+    )}
+    >
       {children}
     </div>
   );
