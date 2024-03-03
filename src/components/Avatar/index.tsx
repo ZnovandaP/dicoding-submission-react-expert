100ad38d7795c077bfb5cb45e18b6b8c906ff4ff
ZnovandaP <znovandaputra@gmail.com>
2024-03-03 21:39:10 +0700

    fix(fixing condition rendering Avatar component): fix condition rendering when using compoenent Avatar with props size

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -26,7 +26,7 @@ export default function Avatar({
 
         size === 'md' && 'h-12 w-12',
 
-        size === 'md' && 'h-16 w-16',
+        size === 'lg' && 'h-16 w-16',
 
         size === 'xl' && 'h-20 w-20',
 
