848e2496d5618c7c2fc0bce31d254b0d165d5dae
ZnovandaP <znovandaputra@gmail.com>
2024-03-04 06:18:47 +0700

    fix(fix wrong endpoint) fixing endpoint on service leaderboards

--- src/service/leaderboards/index.ts
+++ src/service/leaderboards/index.ts
@@ -3,7 +3,7 @@ import { baseUrl } from '../common/fetch-with-auth';
 
 const getLeaderboards = async () => {
   try {
-    const { data } = await axios.get(`${baseUrl}/leaderboard`);
+    const { data } = await axios.get(`${baseUrl}/leaderboards`);
     return data;
   } catch (error: any) {
     throw new Error(error.message);
