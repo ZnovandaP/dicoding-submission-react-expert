dd90c0e9686e2d8b04cfa0ac57c4edef3391fb76
ZnovandaP <znovandaputra@gmail.com>
2024-03-03 21:44:45 +0700

    feat(create component LeaderboardsViews): this component to be use on LeaderboaradsPage

---
+++ src/views/LeaderboardsViews/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import * as React from 'react';
+import usePreload from '@/hooks/usePreload';
+import { useAppSelector } from '@/libs/redux/store';
+import { asyncGetLeaderboards } from '@/libs/redux/slices/leaderboards';
+import { asyncGetProfile } from '@/libs/redux/slices/users/get-own-profile';
+import LeaderboardSection from './ComponentSupport/LeaderboardSection';
+import LeaderboardHeadSection from './ComponentSupport/LeaderboardHeadSection';
+import LeadeboardLoading from './ComponentSupport/UILoading';
+
+export default function LeaderboardsViews() {
+  usePreload([asyncGetLeaderboards, asyncGetProfile]);
+  const { status, data } = useAppSelector((state) => state.leaderboards);
+  return (
+    <div className="flex flex-col gap-8 mt-8 min-h-[80dvh] pb-8">
+      <LeaderboardHeadSection />
+
+      {status === 'success' && (
+      <LeaderboardSection leaderboards={data!} />
+      )}
+
+      <LeadeboardLoading status={status as 'loading'} />
+    </div>
+  );
+}
