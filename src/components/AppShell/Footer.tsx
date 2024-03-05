81ae52d9f29bab801a54c8b375f67980ee642cdc
ZnovandaP <znovandaputra@gmail.com>
2024-03-05 19:45:13 +0700

    fix(responsive on Footer,tsx component)

--- src/components/AppShell/Footer.tsx
+++ src/components/AppShell/Footer.tsx
@@ -6,14 +6,14 @@ import Container from '../Container';
 export default function Footer() {
   return (
     <Container className="relative w-full h-full">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2]">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2] w-[85%] md:w-[60%] lg:w-[45%]">
         <h2 className="center flex-col gap-3 text-2xl md:text-3xl lg:text-4xl text-center text-pretty font-kaushan bg-neutral-50/10 backdrop-blur-xl p-6 px-8 rounded-lg shadow-xl shadow-primary">
           &copy; Sharing Sepuh -
           {' '}
           {new Date().getFullYear()}
 
           <Link
-            className="text-xl font-inter underline text-white"
+            className="text-base md:text-xl font-inter underline text-white"
             href="https://znovandap.com"
             target="_blank"
           >
