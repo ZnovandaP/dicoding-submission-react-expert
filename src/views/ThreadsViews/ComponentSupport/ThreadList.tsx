7e66ba903cb053bf58c4bc5cd912c5743ed8c4ba
ZnovandaP <znovandaputra@gmail.com>
2024-03-05 19:19:55 +0700

    fix(logic conditional rendering thread by category): fix logic conditional rendering and mount NotFoundThreadfor telling the user when the threads is not found by category

--- src/views/ThreadsViews/ComponentSupport/ThreadList.tsx
+++ src/views/ThreadsViews/ComponentSupport/ThreadList.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { useAppSelector } from '@/libs/redux/store';
 import { useSearchParams } from 'next/navigation';
 import ThreadCard from '@/components/Card/Thread';
+import NotFoundThreads from '@/views/NotFoundViews/NotFoundThreads';
 import { ThreadsLoading } from './UILoading';
 
 export default function ThreadList() {
@@ -12,25 +13,22 @@ export default function ThreadList() {
 
   const getAllCategories = searchParams.getAll('category');
 
+  const threadsBasedOnCategories = data?.filter(
+    (thread) => (getAllCategories.length > 0 ? getAllCategories.includes(thread.category) : thread),
+  )!;
+  // console.log(threadsBasedOnCategories);
   return (
     <>
       <ThreadsLoading status={status as 'loading'} />
 
       {status === 'success' && (
-        <div className="mt-8 flex flex-col gap-4 lg:w-[60%]">
-          {data && data?.map((thread) => {
-            if (getAllCategories.length > 0) {
-              if (searchParams.getAll('category')?.includes(thread.category)) {
-                return (
-                  <ThreadCard thread={thread} key={thread.id} />
-                );
-              }
+      <div className="relative mt-8 flex flex-col gap-4 lg:w-[60%]">
+        {threadsBasedOnCategories?.length > 0 && threadsBasedOnCategories?.map((thread) => (
+          <ThreadCard thread={thread} key={thread.id} />
+        ))}
 
-              return null;
-            }
-            return <ThreadCard thread={thread} key={thread.id} />;
-          })}
-        </div>
+        {threadsBasedOnCategories.length < 1 && (<NotFoundThreads />)}
+      </div>
       )}
     </>
   );
